df49c8299fa87690b251e25e515982549fe9dbe2
pavel.sknar <pavel.sknar@gmail.com>
2013-07-29 06:41:43 +0000

    EIRC Account CRUID

--- eirc-account/src/main/java/ru/flexpay/eirc/eirc_account/strategy/EircAccountStrategy.java
+++ eirc-account/src/main/java/ru/flexpay/eirc/eirc_account/strategy/EircAccountStrategy.java
@@ -1,15 +1,16 @@
 package ru.flexpay.eirc.eirc_account.strategy;
 
+import org.apache.commons.lang.builder.EqualsBuilder;
 import org.complitex.dictionary.entity.FilterWrapper;
 import org.complitex.dictionary.mybatis.Transactional;
 import org.complitex.dictionary.service.AbstractBean;
+import org.complitex.dictionary.service.SequenceBean;
 import ru.flexpay.eirc.eirc_account.entity.EircAccount;
 
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import java.util.Date;
 import java.util.List;
-
-import static com.google.common.collect.ImmutableMap.of;
 
 /**
  * @author Pavel Sknar
@@ -20,18 +21,52 @@ public class EircAccountStrategy extends AbstractBean {
     private static final String EIRC_ACCOUNT_MAPPING = EircAccountStrategy.class.getPackage().getName() + ".EircAccount";
     public static final String RESOURCE_BUNDLE = EircAccountStrategy.class.getName();
 
-    @Transactional
-    public void archive(EircAccount object, Date endDate) {
+    @EJB
+    private SequenceBean sequenceBean;
 
+    @Transactional
+    public void archive(EircAccount object) {
+        if (object.getEndDate() == null) {
+            object.setEndDate(new Date());
+        }
+        sqlSession().update("updateEndDate", object);
     }
 
-    @Transactional
     public EircAccount findById(long id) {
-       return sqlSession().selectOne(EIRC_ACCOUNT_MAPPING + ".getAddress", id);
+       return sqlSession().selectOne(EIRC_ACCOUNT_MAPPING + ".getEircAccount", id);
     }
 
     public List<EircAccount> find(FilterWrapper<EircAccount> filter) {
         return sqlSession().selectList(EIRC_ACCOUNT_MAPPING + ".selectEircAccounts", filter);
+    }
+
+    @Transactional
+    public void insert(EircAccount eircAccount) {
+        eircAccount.setId(sequenceBean.nextId(getEntityTable()));
+        sqlSession().insert(EIRC_ACCOUNT_MAPPING + ".insert", eircAccount);
+    }
+
+    @Transactional
+    public void update(EircAccount eircAccount) {
+        EircAccount oldObject = findByPkId(eircAccount.getPkId());
+        if (EqualsBuilder.reflectionEquals(oldObject, eircAccount)) {
+            return;
+        }
+        if (oldObject.getBeginDate().after(eircAccount.getBeginDate())) {
+            throw new RuntimeException("Can not update EIRC Account. Fail date. Old Object: " + oldObject +
+                    ", New Object: " + eircAccount + ".");
+        }
+        oldObject.setEndDate(eircAccount.getBeginDate());
+        archive(oldObject);
+        insert(eircAccount);
+    }
+
+    public static String getEntityTable() {
+        return "eirc_account";
+    }
+
+    private EircAccount findByPkId(long pkId) {
+        return sqlSession().selectOne(EIRC_ACCOUNT_MAPPING + ".getEircAccount", pkId);
     }
 
 }
