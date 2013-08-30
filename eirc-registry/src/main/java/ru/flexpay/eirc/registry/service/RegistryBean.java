98305b7b77b8adcca81652135730523527f5e255
pavel.sknar <pavel.sknar@gmail.com>
2013-08-30 07:53:29 +0000

    Working on Registry beans

--- eirc-registry/src/main/java/ru/flexpay/eirc/registry/service/RegistryBean.java
+++ eirc-registry/src/main/java/ru/flexpay/eirc/registry/service/RegistryBean.java
@@ -1,11 +1,46 @@
 package ru.flexpay.eirc.registry.service;
 
+import org.complitex.dictionary.entity.FilterWrapper;
+import org.complitex.dictionary.mybatis.Transactional;
+import org.complitex.dictionary.service.AbstractBean;
+import ru.flexpay.eirc.registry.entity.Container;
+import ru.flexpay.eirc.registry.entity.Registry;
+
 import javax.ejb.Stateless;
+import java.util.List;
 
 /**
  * @author Pavel Sknar
  */
 @Stateless
-public class RegistryBean {
+public class RegistryBean extends AbstractBean {
     private static final String NS = RegistryBean.class.getName();
+
+    public List<Registry> getRegistries(FilterWrapper<Registry> filter) {
+        return getSqlSessionManager().selectList(NS + ".selectRegistries", filter);
+    }
+
+    @Transactional
+    public void save(Registry registry) {
+        getSqlSessionManager().insert(NS + ".insertRegistry", registry);
+
+        RegistryContainer registryContainer = new RegistryContainer(registry.getId());
+
+        for (Container container : registry.getContainers()) {
+            registryContainer.setContainer(container);
+            getSqlSessionManager().insert(NS + ".insertRegistryContainer", registryContainer);
+        }
+    }
+
+    public class RegistryContainer extends ContainerProxy {
+        private Long registryId;
+
+        public RegistryContainer(Long registryId) {
+            this.registryId = registryId;
+        }
+
+        public Long getRegistryId() {
+            return registryId;
+        }
+    }
 }
--- eirc-registry/src/main/java/ru/flexpay/eirc/registry/service/RegistryRecordBean.java
+++ eirc-registry/src/main/java/ru/flexpay/eirc/registry/service/RegistryRecordBean.java
@@ -1,11 +1,54 @@
 package ru.flexpay.eirc.registry.service;
 
+import org.complitex.dictionary.entity.FilterWrapper;
+import org.complitex.dictionary.mybatis.Transactional;
+import org.complitex.dictionary.service.AbstractBean;
+import ru.flexpay.eirc.registry.entity.Container;
+import ru.flexpay.eirc.registry.entity.Registry;
+import ru.flexpay.eirc.registry.entity.RegistryRecord;
+
 import javax.ejb.Stateless;
+import java.util.List;
 
 /**
  * @author Pavel Sknar
  */
 @Stateless
-public class RegistryRecordBean {
+public class RegistryRecordBean extends AbstractBean {
     private static final String NS = RegistryRecordBean.class.getName();
+
+    public List<Registry> getRegistryRecords(FilterWrapper<RegistryRecord> filter) {
+        return getSqlSessionManager().selectList(NS + ".selectRegistryRecords", filter);
+    }
+
+    @Transactional
+    public void saveBulk(List<RegistryRecord> registryRecords) {
+        for (RegistryRecord registryRecord : registryRecords) {
+            save(registryRecord);
+        }
+    }
+
+    @Transactional
+    public void save(RegistryRecord registryRecord) {
+        getSqlSessionManager().insert(NS + ".insertRegistryRecord", registryRecord);
+
+        RegistryRecordContainer registryContainer = new RegistryRecordContainer(registryRecord.getId());
+
+        for (Container container : registryRecord.getContainers()) {
+            registryContainer.setContainer(container);
+            getSqlSessionManager().insert(NS + ".insertRegistryRecordContainer", registryContainer);
+        }
+    }
+
+    public class RegistryRecordContainer extends ContainerProxy {
+        private Long registryRecordId;
+
+        public RegistryRecordContainer(Long registryRecordId) {
+            this.registryRecordId = registryRecordId;
+        }
+
+        public Long getRegistryRecordId() {
+            return registryRecordId;
+        }
+    }
 }
