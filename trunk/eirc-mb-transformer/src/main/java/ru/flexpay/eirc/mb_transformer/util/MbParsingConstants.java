1e554cd103f250709cc4560bbc2661c8ce318689
pavel.sknar@gmail.com <pavel.sknar@gmail.com@e8fd8551-922e-a84f-f546-1090034d8034>
2014-01-10 12:43:46 +0000

    1. Refactoring bulk insert. 2. Escaped delimiter chars. 3. Fixed date errors.

--- trunk/eirc-mb-transformer/src/main/java/ru/flexpay/eirc/mb_transformer/util/MbParsingConstants.java
+++ trunk/eirc-mb-transformer/src/main/java/ru/flexpay/eirc/mb_transformer/util/MbParsingConstants.java
@@ -1,22 +1,23 @@
 package ru.flexpay.eirc.mb_transformer.util;
 
-import java.text.SimpleDateFormat;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
 public abstract class MbParsingConstants {
     public static final String LAST_FILE_STRING_BEGIN = "999999999";
 	public static final String REGISTRY_FILE_ENCODING = "Cp866";
     public static final String BUILDING_BULK_PREFIX = "К";
-    public static final SimpleDateFormat FILE_CREATION_DATE_FORMAT = new SimpleDateFormat("ddMMyy");
-    public static final SimpleDateFormat OPERATION_MONTH_DATE_FORMAT = new SimpleDateFormat("MMyy");
+    public static final DateTimeFormatter FILE_CREATION_DATE_FORMAT = DateTimeFormat.forPattern("ddMMyy");
+    public static final DateTimeFormatter OPERATION_MONTH_DATE_FORMAT = DateTimeFormat.forPattern("MMyy");
     public static final String FIRST_FILE_STRING =
 			"                                                                                                    "
 			+ "                                                                                                    "
 			+ "                                                                                                    ";
 	public static final int FIRST_FILE_STRING_SIZE = 300;
 
-    public static final SimpleDateFormat CORRECTIONS_MODIFICATIONS_START_DATE_FORMAT = new SimpleDateFormat("ddMMyy");
-    public static final SimpleDateFormat CHARGES_MODIFICATIONS_START_DATE_FORMAT = new SimpleDateFormat("MMyy");
-    public static final SimpleDateFormat OPERATION_DATE_FORMAT = new SimpleDateFormat("ddMMyyyy");
+    public static final DateTimeFormatter CORRECTIONS_MODIFICATIONS_START_DATE_FORMAT = DateTimeFormat.forPattern("ddMMyy");
+    public static final DateTimeFormatter CHARGES_MODIFICATIONS_START_DATE_FORMAT = DateTimeFormat.forPattern("MMyy");
+    public static final DateTimeFormatter OPERATION_DATE_FORMAT = DateTimeFormat.forPattern("ddMMyyyy");
 
     public static final String DELIMITER = "=";
 
