a9ee321774313434f21a0b0f74aa403fe2422dce
pavel.sknar <pavel.sknar@gmail.com>
2014-02-21 12:40:55 +0000

    visualization of the organization in transformer config

---
+++ eirc-mb-transformer/src/main/java/ru/flexpay/eirc/mb_transformer/web/component/OrganizationPicker.java
@@ -0,0 +1,32 @@
+package ru.flexpay.eirc.mb_transformer.web.component;
+
+import org.apache.ibatis.session.SqlSessionManager;
+import org.apache.wicket.model.IModel;
+import org.complitex.dictionary.mybatis.SqlSessionFactoryBean;
+import org.complitex.dictionary.service.ConfigBean;
+import ru.flexpay.eirc.mb_transformer.entity.MbTransformerConfig;
+
+import javax.ejb.EJB;
+
+/**
+ * @author Pavel Sknar
+ */
+public class OrganizationPicker extends org.complitex.organization.web.component.OrganizationPicker {
+
+    @EJB
+    private ConfigBean configBean;
+
+    public OrganizationPicker(String id, IModel<String> model, Long organizationTypeId) {
+        super(id, model, organizationTypeId);
+
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean() {
+            @Override
+            public SqlSessionManager getSqlSessionManager() {
+                return getSqlSessionManager(configBean.getString(MbTransformerConfig.EIRC_DATA_SOURCE), "remote");
+            }
+        };
+
+
+        organizationStrategy.setSqlSessionFactoryBean(sqlSessionFactoryBean);
+    }
+}
